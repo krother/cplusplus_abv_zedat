36d488f35b8b82647d540cdf0d526a95a09d2c57
Kristian Rother <krother@academis.eu>
2017-11-20 08:46:26 +0100

    2 kleine Bugs repariert

--- projekte_ss2017/das_ziel_ist_der_weg/code.cpp
+++ projekte_ss2017/das_ziel_ist_der_weg/code.cpp
@@ -14,7 +14,7 @@ int main(int x, char** y) {
     // Bilder laden
     IMG_Init( IMG_INIT_PNG );
     SDL_Surface *picture1 = IMG_Load("bilder/Picture1NN.png");
-    SDL_Surface *picture2 = IMG_Load("bilder/Picture2NN.png");
+    SDL_Surface *picture2 = IMG_Load("bilder/picture2NN.png");
     SDL_Surface *picture3 = IMG_Load("bilder/picture3N.png");
     SDL_Surface *picture4 = IMG_Load("bilder/picture4N.png");
     SDL_Surface *picture5 = IMG_Load("bilder/Picture5N.png");
@@ -30,7 +30,7 @@ int main(int x, char** y) {
     SDL_Surface *picture15 = IMG_Load("bilder/AugeN.png");
     SDL_Surface *picture16 = IMG_Load("bilder/Picture6Nend.png");
     SDL_Surface *picture17 = IMG_Load("bilder/Picture11Nend.png");
-    SDL_Surface *picture18 = IMG_Load("bilder/Picture12Nend.png");
+    SDL_Surface *picture18 = IMG_Load("bilder/picture12Nend.png");
 
     SDL_Surface *FruhstuckStand = IMG_Load("bilder/FruhstuckStand.png");
     SDL_Surface *StopuhrStand = IMG_Load("bilder/StopuhrStand.png");
@@ -39,14 +39,15 @@ int main(int x, char** y) {
     string eingabe;
     int position = 1;
     bool fruhstuck = false;
-	bool ticket = false;
-	bool verspatung = false;
-	int answer;
+    bool ticket = false;
+    bool verspatung = false;
+    int answer;
 
     SDL_Event e;
     int taste = 0;
     do {
-    			while( SDL_PollEvent( &e ) != 0 ) {
+                answer = 3;
+                while( SDL_PollEvent( &e ) != 0 ) {
 
                 if( e.type == SDL_KEYDOWN ) {
                     taste = e.key.keysym.sym;
@@ -58,217 +59,218 @@ int main(int x, char** y) {
                     }
                 }
 
+                if (position == 1) {
+                SDL_BlitSurface(picture1, NULL, surf, NULL);
+                fruhstuck = false;
+                ticket = false;
+                verspatung = false;
+                         if (answer == true) {
+                            position += 1;
+                            answer = 3;                //Hilfsmittel!
+                        }
+                        else if (answer == false) {
+                            position += 6;
+                            answer = 3;
+                        }
+                }
+                if (position == 2) {
+                SDL_BlitSurface(picture2, NULL, surf, NULL);
+                        if (answer == true) {
+                               position += 1;
+                               fruhstuck = true;
+                               answer = 3;
+                        }
+                        else if (answer == false) {
+                            position += 1;
+                            answer = 3;
+                         }
+                }
+                else if (position == 3) {
+                SDL_BlitSurface(picture3, NULL, surf, NULL);
+                        if (answer == true) {
+                            position += 1;
+                            ticket = true;
+                            verspatung = true;
+                            answer = 3;
+                        }
+                        else if (answer == false) {
+                            position += 1;
+                            answer = 3;
+                        }
+                }
+                else if (position == 4) {
+                SDL_BlitSurface(picture4, NULL, surf, NULL);
+                        if (ticket == true && answer == true) {
+                            position += 1;
+                            answer = 3;
+                        }
+                        else if (ticket == false && answer == false) {
+                            position += 4;
+                            answer = 3;
+                        }
+                        else if (ticket == false && answer == true) {
+                            position += 11;
+                            answer = 3;
+                        }
+                        else if (ticket == true && answer == false) {
+                            position += 11;
+                            answer = 3;
+                        }
+                 }
+                else if (position == 5) {
+                SDL_BlitSurface(picture5, NULL, surf, NULL);
+                        if (answer == true) {
+                            position += 1;
+                               answer = 3;
+                        }
+                        else if (answer == false) {
+                               position += 9;
+                               answer = 3;
+                        }
+                }
+                else if (position == 6) {
+                        SDL_BlitSurface(picture6, NULL, surf, NULL);
+                        if (answer == true){
+                            position += 10;
+                            answer = 3;
+                        }
+                        else if (answer == false) {
+                            position -= 5;
+                            answer = 3;
+                        }
+                }
+                else if (position == 7) {
+                        SDL_BlitSurface(picture7, NULL, surf, NULL);
+                        if (answer == true){
+                            position += 7;
+                            answer = 3;
+                        }
+                }
+                else if (position == 8) {
+                        SDL_BlitSurface(picture8, NULL, surf, NULL);
+                        if (answer == true && fruhstuck == true) {
+                            position += 5;
+                            answer = 3;
+                        }
+                        else if (answer == false && fruhstuck == false) {
+                            position += 1;
+                            answer = 3;
+                        }
+                        else if (fruhstuck == false && answer == true) {
+                            position += 7;
+                            answer = 3;
+                        }
+                        else if (fruhstuck == true && answer == false) {
+                            position += 7;
+                            answer = 3;
+                        }
+                }
+                else if (position == 9) {
+                        SDL_BlitSurface(picture9, NULL, surf, NULL);
+                        if (answer == true) {
+                            position += 1;
+                            answer = 3;
+                        }
+                         else if (answer == false) {
+                             position += 3;
+                             answer = 3;
+                         }
+                }
+                else if (position == 10) {
+                        SDL_BlitSurface(picture10, NULL, surf, NULL);
+                        if (answer == true) {
+                            position += 1;
+                            answer = 3;
+                        }
+                        else if (answer == false){
+                            position += 4;
+                            answer = 3;
+                        }
+                }
+                else if (position == 11) {
+                        SDL_BlitSurface(picture11, NULL, surf, NULL);
+                        if (answer == true){
+                            position += 6;
+                            answer = 3;
+                        }
+                        else if (answer == false) {
+                            position -= 10;
+                            answer = 3;
+                        }
+                }
+                else if (position == 12) {
+                        SDL_BlitSurface(picture12, NULL, surf, NULL);
+                        if (answer == true){
+                            position += 6;
+                            answer = 3;
+                        }
+                        else if (answer == false) {
+                            position -= 11;
+                            answer = 3;
+                        }
+                }
+                else if (position == 13) {
+                           SDL_BlitSurface(picture13, NULL, surf, NULL);
+                           verspatung = true;
+                           if (answer == true) {
+                               position -= 8;
+                               answer = 3;
+                           }
+                }
+                else if (position == 14){
+                    SDL_BlitSurface(picture14, NULL, surf, NULL);
+                    fruhstuck = false;
+                    ticket = false;
+                    verspatung = false;
+                    if (answer == true) {
+                        position -=13;
+                        answer = 3;
+                    }
+                    else if (answer == false) {
+                        position -=13;
+                        answer = 3;
+                    }
+                }
+                else if (position == 15){
+                    SDL_BlitSurface(picture15, NULL, surf, NULL);
+                    fruhstuck = false;
+                    ticket = false;
+                    verspatung = false;
+                    if (answer == true){
+                        position -= 14;
+                        answer = 3;
+                    }
+                }
+                else if (position == 16){
+                SDL_BlitSurface(picture16, NULL, surf, NULL);
+                SDL_Delay(5000);
+                }
+                else if (position == 17){
+                SDL_BlitSurface(picture17, NULL, surf, NULL);
+                SDL_Delay(5000);
+                }
+                else if (position == 18){
+                SDL_BlitSurface(picture18, NULL, surf, NULL);
+                SDL_Delay(5000);
+                }
 
-    			 	 	 	if (position == 1) {
-    			            SDL_BlitSurface(picture1, NULL, surf, NULL);
-    			            fruhstuck = false;
-    			            ticket = false;
-    			            verspatung = false;
-    			                     if (answer == true) {
-    			                    	position += 1;
-    			                    	answer = 3;				//Hilfsmittel!
-    			                    }
-    			                    else if (answer == false) {
-    			                    	position += 6;
-    			                    	answer = 3;
-    			                    }
-    			            }
-    			            if (position == 2) {
-    			            SDL_BlitSurface(picture2, NULL, surf, NULL);
-    			                    if (answer == true) {
-    			                       	position += 1;
-    			                       	fruhstuck = true;
-    			                       	answer = 3;
-    			                    }
-    			                    else if (answer == false) {
-    			                    	position += 1;
-    			                    	answer = 3;
-    			                     }
-    			            }
-    			            else if (position == 3) {
-    			            SDL_BlitSurface(picture3, NULL, surf, NULL);
-    			                    if (answer == true) {
-    			                        position += 1;
-    			                        ticket = true;
-    			                        verspatung = true;
-    			                        answer = 3;
-    			                    }
-    			                    else if (answer == false) {
-    			                        position += 1;
-    			                        answer = 3;
-    			                    }
-    			            }
-    			            else if (position == 4) {
-    			            SDL_BlitSurface(picture4, NULL, surf, NULL);
-    			                    if (ticket == true && answer == true) {
-    			                    	position += 1;
-    			                    	answer = 3;
-    			                	}
-    			                	else if (ticket == false && answer == false) {
-    			                		position += 4;
-    			                		answer = 3;
-    			                	}
-    			                	else if (ticket == false && answer == true) {
-    			                		position += 11;
-    			                		answer = 3;
-    			                	}
-    			                	else if (ticket == true && answer == false) {
-    			                		position += 11;
-    			                		answer = 3;
-    			                	}
-    			             }
-    			            else if (position == 5) {
-    			            SDL_BlitSurface(picture5, NULL, surf, NULL);
-    			            		if (answer == true) {
-    			            			position += 1;
-    			               			answer = 3;
-    			            		}
-    			            		else if (answer == false) {
-    			               			position += 9;
-    			               			answer = 3;
-    			            		}
-    			            }
-    			            else if (position == 6) {
-    			            		SDL_BlitSurface(picture6, NULL, surf, NULL);
-    			            		if (answer == true){
-    			            			position += 10;
-    			            			answer = 3;
-    			            		}
-    			            		else if (answer == false) {
-    			            			position -= 5;
-    			            			answer = 3;
-    			            		}
-    			            }
-    			            else if (position == 7) {
-    			                	SDL_BlitSurface(picture7, NULL, surf, NULL);
-    			                	if (answer == true){
-    			                		position += 7;
-    			                		answer = 3;
-    			                	}
-    			            }
-    			            else if (position == 8) {
-    			            		SDL_BlitSurface(picture8, NULL, surf, NULL);
-    			            		if (answer == true && fruhstuck == true) {
-    			            			position += 5;
-    			            			answer = 3;
-    			            		}
-    			            		else if (answer == false && fruhstuck == false) {
-    			            			position += 1;
-    			            			answer = 3;
-    			            		}
-    			            		else if (fruhstuck == false && answer == true) {
-    			            		    position += 7;
-    			            		    answer = 3;
-    			            		}
-    			            		else if (fruhstuck == true && answer == false) {
-    			            		    position += 7;
-    			            		    answer = 3;
-    			            		}
-    			            }
-    			            else if (position == 9) {
-    			                	SDL_BlitSurface(picture9, NULL, surf, NULL);
-    			                	if (answer == true) {
-    			                		position += 1;
-    			                		answer = 3;
-    			                	}
-    			                	 else if (answer == false) {
-    			                		 position += 3;
-    			                		 answer = 3;
-    			                	 }
-    			            }
-    			            else if (position == 10) {
-    			                	SDL_BlitSurface(picture10, NULL, surf, NULL);
-    			                	if (answer == true) {
-    			                		position += 1;
-    			                		answer = 3;
-    			                	}
-    			                	else if (answer == false){
-    			                		position += 4;
-    			                		answer = 3;
-    			                	}
-    			            }
-    			            else if (position == 11) {
-    			                	SDL_BlitSurface(picture11, NULL, surf, NULL);
-    			                	if (answer == true){
-    			                		position += 6;
-    			                	    answer = 3;
-    			                	}
-    			                	else if (answer == false) {
-    			                	    position -= 10;
-    			                	    answer = 3;
-    			                	}
-    			            }
-    			            else if (position == 12) {
-    			                	SDL_BlitSurface(picture12, NULL, surf, NULL);
-    			                	if (answer == true){
-    			                		position += 6;
-    			                		answer = 3;
-    			                    }
-    			                	else if (answer == false) {
-    			                    	position -= 11;
-    			                    	answer = 3;
-    			                    }
-    			            }
-    			            else if (position == 13) {
-    			               		SDL_BlitSurface(picture13, NULL, surf, NULL);
-    			               		verspatung = true;
-    			               		if (answer == true) {
-    			               			position -= 8;
-    			               			answer = 3;
-    			               		}
-    			            }
-    			            else if (position == 14){
-    			            	SDL_BlitSurface(picture14, NULL, surf, NULL);
-    			            	fruhstuck = false;
-    			            	ticket = false;
-    			            	verspatung = false;
-    			            	if (answer == true) {
-    			            		position -=13;
-    			            		answer = 3;
-    			            	}
-    			            	else if (answer == false) {
-    			            		position -=13;
-    			            		answer = 3;
-    			            	}
-    			            }
-    			            else if (position == 15){
-    			            	SDL_BlitSurface(picture15, NULL, surf, NULL);
-    			            	fruhstuck = false;
-    			            	ticket = false;
-    			            	verspatung = false;
-    			            	if (answer == true){
-    			            	    position -= 14;
-    			            	    answer = 3;
-    			            	}
-    			            }
-    			            else if (position == 16){
-    			            SDL_BlitSurface(picture16, NULL, surf, NULL);
-    			            }
-    			            else if (position == 17){
-    			            SDL_BlitSurface(picture17, NULL, surf, NULL);
-    			            }
-    			            else if (position == 18){
-    			            SDL_BlitSurface(picture18, NULL, surf, NULL);
-    			            }
 
+                if (fruhstuck == true){
+                SDL_BlitSurface(FruhstuckStand, NULL, surf, NULL);
+                }
+                if (ticket == true){
+                SDL_BlitSurface(TicketStand, NULL, surf, NULL);
+                }
+                if (verspatung == true){
+                SDL_BlitSurface(StopuhrStand, NULL, surf, NULL);
+                }
 
-    	if (fruhstuck == true){
-    	SDL_BlitSurface(FruhstuckStand, NULL, surf, NULL);
-    	}
-    	if (ticket == true){
-    	SDL_BlitSurface(TicketStand, NULL, surf, NULL);
-    	}
-    	if (verspatung == true){
-    	SDL_BlitSurface(StopuhrStand, NULL, surf, NULL);
-    	}
-    			 	 	 	SDL_UpdateWindowSurface(win);
-    			 	 	 	SDL_Delay(200);
-
-}
+                SDL_UpdateWindowSurface(win);
+                SDL_Delay(20);
+            }
     }
-while (position < 16);
-return 0;
+    while (position < 16);
+    return 0;
+
 }
-
-
 
